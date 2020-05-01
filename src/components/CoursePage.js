fc4970e712f00c18b85ab9ca86271cfe52aeced9
Георги <gtk960311@gmail.com>
2020-05-02 00:07:02 +0300

    Changing from Classes to Hooks and function components. Added Composed components for separation of concerns

--- src/components/CoursePage.js
+++ src/components/CoursePage.js
@@ -1,40 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { getCourses } from "../api/courseApi";
+import CourseList from './CourseList';
 
-class CoursePage extends React.Component {
-    state = {
-        courses: []
-    };
+const CoursePage = () => {
+    let [courses, setCourses] = useState([])
 
-    componentDidMount() {
-        getCourses().then(courses => this.setState({ courses: courses }));
-    }
+    useEffect(() => {
+        getCourses().then(courses => setCourses(courses));
+    }, []);
 
-    render() {
-        return <>
-            <h2>Courses</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Author ID</th>
-                        <th>Category</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.courses.map(course => {
-                        return (
-                            <tr key={course.id}>
-                                <td>{course.title}</td>
-                                <td>{course.authorId}</td>
-                                <td>{course.category}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </>
-    }
+    return <>
+        <h2>Courses</h2>
+        <CourseList courses={courses} />
+    </>
 }
 
 export default CoursePage;
