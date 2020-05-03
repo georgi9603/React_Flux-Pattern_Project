0dc950def399c083c79bfd3fce9e270cd0fd0d06
Георги <gtk960311@gmail.com>
2020-05-03 22:55:42 +0300

    Implementing the save button and redirecting via history push to props.

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import CourseForm from './CourseForm';
+import * as courseApi from '../api/courseApi';
 
 const ManageCoursePage = props => {
     const [course, setCourse] = useState({
@@ -12,10 +13,17 @@ const ManageCoursePage = props => {
 
     const handleChange = ({ target }) => { setCourse({ ...course, [target.name]: target.value }) }
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        courseApi.saveCourse(course).then(() => {
+            props.history.push("/courses");
+        });
+    }
+
     return (
         <>
             <h2>Manage Course</h2>
-            <CourseForm course={course} onChange={handleChange} />
+            <CourseForm course={course} onChange={handleChange} onSubmit={handleSubmit} />
         </>
     )
 }
