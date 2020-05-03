a5cd2ed13f5096bafeab68d29642fce5b27021a2
Георги <gtk960311@gmail.com>
2020-05-03 23:02:07 +0300

    Adding 3rd party notification after save with react-toastify.

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import CoursesPage from './CoursePage';
 import PageNotFound from './PageNotFound';
 import ManageCoursePage from "./ManageCoursePage";
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";;
 
 function App() {
     return (
         <div className="container-fluid">
+            <ToastContainer autoClose={3000} hideProgressBar />
             <Header />
             <Switch>
                 <Route exact path='/' component={HomePage} />
